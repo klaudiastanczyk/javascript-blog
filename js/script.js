cf9310c826f83159b2961cfef183c9ef056f199e
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-03-23 19:41:10 +0100

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 
 function titleClickHandler(event){
-    console.log(event, 'Link was clicked!');
+    const clickedElement = this;
+    console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +13,12 @@ function titleClickHandler(event){
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);
+    this.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post');
 
