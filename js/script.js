7c0fa7106bc8d17311f79bf62822c382a5209c27
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-03-25 14:21:21 +0100

    create HTML of the link

--- js/script.js
+++ js/script.js
@@ -47,4 +47,51 @@ function titleClickHandler(event){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+
   
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+    
+    /* get the article id */
+
+    const articleID = document.getAttribute('id');
+
+    /* find the title element */
+
+    /*const titleElement = document.querySelector(optTitleListSelector);*/
+
+    /* get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleListSelector).innerHTML;
+  
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into titleList */
+
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+  }
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
